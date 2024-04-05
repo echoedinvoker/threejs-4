04ff8a23619ccaf28a438e72db3c7a4adca5e4e0
Matt Chang <echoedinvoker@gmail.com>
2024-04-05 14:58:38 +0800

    9: save two glb.scene in an array instead of adding them to scene directly

--- src/script.js
+++ src/script.js
@@ -62,49 +62,51 @@ const firstModelColor2 = 'yellow'
 const secondModelColor1 = 'blue'
 const secondModelColor2 = 'white'
 
+const modelArray = []
+
 //Loading Models
 //1)
-// gltfLoader.load("/models/1/1.glb", (glb) => {
-//
-//   //increase the number of particles
-//   const samplerMesh = new MeshSurfaceSampler(glb.scene.children[0]).build();
-//   const numParticles = 20000;
-//   const particlesGeometry = new THREE.BufferGeometry();
-//   const particlesArray = new Float32Array(numParticles * 3);
-//   for (let i = 0; i < numParticles; i++) {
-//     const position = new THREE.Vector3();
-//     samplerMesh.sample(position);
-//     particlesArray.set(
-//       [position.x, position.y, position.z],
-//       i * 3
-//     )
-//   }
-//   particlesGeometry.setAttribute(
-//     'position',
-//     new THREE.BufferAttribute(particlesArray, 3)
-//   )
-//
-//
-//   //change model into particles
-//   glb.scene.children[0] = new THREE.Points(
-//     particlesGeometry,
-//     new THREE.RawShaderMaterial({
-//       vertexShader: vShader,
-//       fragmentShader: fShader,
-//       uniforms: {
-//         u_color_1: { value: new THREE.Color(firstModelColor1) },
-//         u_color_2: { value: new THREE.Color(firstModelColor2) },
-//       },
-//       depthTest: false,
-//       blending: THREE.AdditiveBlending,
-//     })
-//   )
-//
-//   glb.scene.children[0].scale.set(0.5, 0.5, 0.5);
-//   glb.scene.children[0].position.x = 0.5;
-//   glb.scene.children[0].rotation.y = Math.PI / 2;
-//   scene.add(glb.scene)
-// })
+gltfLoader.load("/models/1/1.glb", (glb) => {
+
+  //increase the number of particles
+  const samplerMesh = new MeshSurfaceSampler(glb.scene.children[0]).build();
+  const numParticles = 20000;
+  const particlesGeometry = new THREE.BufferGeometry();
+  const particlesArray = new Float32Array(numParticles * 3);
+  for (let i = 0; i < numParticles; i++) {
+    const position = new THREE.Vector3();
+    samplerMesh.sample(position);
+    particlesArray.set(
+      [position.x, position.y, position.z],
+      i * 3
+    )
+  }
+  particlesGeometry.setAttribute(
+    'position',
+    new THREE.BufferAttribute(particlesArray, 3)
+  )
+
+
+  //change model into particles
+  glb.scene.children[0] = new THREE.Points(
+    particlesGeometry,
+    new THREE.RawShaderMaterial({
+      vertexShader: vShader,
+      fragmentShader: fShader,
+      uniforms: {
+        u_color_1: { value: new THREE.Color(firstModelColor1) },
+        u_color_2: { value: new THREE.Color(firstModelColor2) },
+      },
+      depthTest: false,
+      blending: THREE.AdditiveBlending,
+    })
+  )
+
+  glb.scene.children[0].scale.set(0.5, 0.5, 0.5);
+  glb.scene.children[0].position.x = 0.5;
+  glb.scene.children[0].rotation.y = Math.PI / 2;
+  modelArray[0] = glb.scene
+})
 
 //2)
 gltfLoader.load("/models/1/2.glb", (glb) => {
@@ -147,7 +149,7 @@ gltfLoader.load("/models/1/2.glb", (glb) => {
   glb.scene.children[0].rotation.x = -Math.PI / 2.0;
   glb.scene.children[0].position.y = -0.2;
   glb.scene.children[0].rotation.z = -Math.PI / 2.0;
-  scene.add(glb.scene)
+  modelArray[1] = glb.scene
 })
 //OrbitControl
 const orbitControls = new OrbitControls(camera, canvas);
